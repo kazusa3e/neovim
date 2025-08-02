4878ff7e1efb0019cb917f70233de652cdf0cb6d
kazusa <i@kazusa.me>
2025-08-02 22:20:50 +0800

    feat(colorscheme): change colorscheme to 'ayu-mirage' and bump update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,23 @@
 return {
     {
-
         "sainnhe/edge",
+        enabled = false,
         lazy = false,
         config = function()
             vim.g.edge_enable_italic = false
             vim.cmd.colorscheme('edge')
+        end
+    },
+    {
+        "shatur/neovim-ayu",
+        lazy = false,
+        opts = {
+            mirage = true,
+            terminal = true
+        },
+        config = function(opts, _)
+            require 'ayu'.setup(opts)
+            vim.cmd.colorscheme('ayu-mirage')
         end
     }
 }
